c6aea4000b338a2c7cb4d8e56da1dc90bb52bc4c
Vyacheslav <34347@iitu.edu.kz>
2025-02-18 23:34:49 +0500

    step 6 Добавление компонентов AppHeader, SearchPanel и TodoList, рефакторинг компонента App

--- src/index.js
+++ src/index.js
@@ -1,39 +1,15 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
+import AppHeader from "./components/app-header";
+import SearchPanel from "./components/search-panel";
+import TodoList from "./components/todo-list";
 
-const ToDoList= () => {
-  const items = ['Install React', 'Study React', 'Use React', 'Build React App'];
-  return (
-    <ul>
-      <li>{items[0]}</li>
-      <li>{items[1]}</li>
-      <li>{items[2]}</li>
-      <li>{items[3]}</li>
-    </ul>
-  );
-};
-const AppHeader = () => {
-  return <h1>My ToDo List</h1>
-};
-const SearchPanel = () => {
-  const searchText = 'Type here to search';
-  const searchStyle = { fontSize: '20px' };
-
-
-  // return <input placeholder="seratch" />
-  return <input 
-    style = {searchStyle}
-    placeholder = {searchText}
-    disabled = {true} />
-};
 const App = () => {
-  const value = '<script>alert ("")</script>';
   return (
     <div>
-      { value }
       <AppHeader/>
       <SearchPanel/>
-      <ToDoList/>
+      <TodoList />
     </div>
   );
 };
