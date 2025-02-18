e9fc1496f4b1a64dbb9f37cf480f604daf1c4123
Vyacheslav <34347@iitu.edu.kz>
2025-02-18 21:57:52 +0500

    Добавление базовой структуры приложения ToDo с использованием React

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+const element = (
+  <div>
+    <h1>My ToDo List</h1>
+    <input placeholder="search" />
+    <ul>
+      <li>Install React</li>
+      <li>Study React</li>
+      <li>Use React</li>
+      <li>Build React App</li>
+    </ul>
+  </div>
+);
 console.log('Hello, Slava');
+
+const root = ReactDOM.createRoot(document.getElementById('root')); // Get the root DOM element
+root.render(element); // Render the element into the root
